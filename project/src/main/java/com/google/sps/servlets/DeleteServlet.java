1649d3d51b6414fca41386811f0c6b181a9bf087
chloeji8888 <chloeji8888@gmail.com>
2022-07-17 18:57:35 +0000

    added a confrimation for the deletion

--- project/src/main/java/com/google/sps/servlets/DeleteServlet.java
+++ project/src/main/java/com/google/sps/servlets/DeleteServlet.java
@@ -41,7 +41,6 @@ public class DeleteServlet extends HttpServlet {
             servletException.setStackTrace(sqlException.getStackTrace());
             throw servletException;
         }
-
         response.sendRedirect("index.html");
     }
 }
